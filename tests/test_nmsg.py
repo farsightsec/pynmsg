a7ae04a7f64c936c2f33394f3c967820854584f8
Kelvin Dealca <dealca@fsi.io>
2020-12-23 15:16:13 +0000

    Fix multiprocessing errors in unittests on macos

--- tests/test_nmsg.py
+++ tests/test_nmsg.py
@@ -19,8 +19,15 @@ import json
 import time
 import os
 import warnings
-from multiprocessing import Process, Value
+# from multiprocessing import Process, Value
+import multiprocessing as mp
 
+try:
+    # This is needed because 'spawn' is the start method in macos by default since 3.8.
+    mp.set_start_method('fork')
+except AttributeError:
+    # Running in Python2, fork is already the start method
+    pass
 
 data = b'NMSG\x00\x02\x00\x00\x02\x1c\n.\x08\x01\x10\x0b\x18\xe2\xaa\xe5\xe0\x05%\x92\xa0\x93%*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe3\xaa\xe5\xe0\x05%\xcd\x8c\xd0\x07*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe3\xaa\xe5\xe0\x05%T\xc3\xa6%*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe4\xaa\xe5\xe0\x05%\xa2v\xe2\x07*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe4\xaa\xe5\xe0\x05%\xb8\xc0\xb8%*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe5\xaa\xe5\xe0\x05%\x0b$\xf5\x07*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe5\xaa\xe5\xe0\x05%\x04\x14\xcd%*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe6\xaa\xe5\xe0\x05%\xd4\x92\x05\x08*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe6\xaa\xe5\xe0\x05%\xe9\xdc\xdb%*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\n.\x08\x01\x10\x0b\x18\xe7\xaa\xe5\xe0\x05%\xc7\xa3\x17\x08*\x17\x08\x00\x12\x13"FSI SIE heartbeat"8\xfd\xd9\x80\xdd\x01\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08\x10\xac\x8c\xd8\xff\x08'
 
@@ -95,7 +102,7 @@ class TestNMSG(unittest.TestCase):
             self.assertEqual(j['message']['type'], "TEXT")
             self.assertEqual(j['message']['payload'], "IkZTSSBTSUUgaGVhcnRiZWF0Ig==")
 
-        p = Process(target=reader)
+        p = mp.Process(target=reader)
         p.start()
         time.sleep(1)
         s = nmsg.output.open_sock('127.0.0.1', 19191)
@@ -120,7 +127,7 @@ class TestNMSG(unittest.TestCase):
                 self.assertEqual(j['message']['type'], "TEXT")
                 self.assertEqual(j['message']['payload'], "IkZTSSBTSUUgaGVhcnRiZWF0Ig==")
 
-        p = Process(target=reader)
+        p = mp.Process(target=reader)
         p.start()
         s.write(mlist[0])
         s.flush()
@@ -172,8 +179,8 @@ class TestNMSG(unittest.TestCase):
             self.assertEqual(j['message']['type'], "TEXT")
             self.assertEqual(j['message']['payload'], "IkZTSSBTSUUgaGVhcnRiZWF0Ig==")
 
-        num = Value('d', 0)
-        p = Process(target=reader, args=(num,))
+        num = mp.Value('d', 0)
+        p = mp.Process(target=reader, args=(num,))
         p.start()
         s = nmsg.output.open_file(_wfo)
         while num.value == 0:
