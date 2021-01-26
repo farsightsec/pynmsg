b648f3f84f9067adb5607029cb07041d7fa66ae6
Allan LeSage <alesage@fsi.io>
2021-01-26 00:49:59 +0000

    Some copyright and minor tweaks for 0.5.0 release.

--- examples/nmsg_nullinput.py
+++ examples/nmsg_nullinput.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2018-2019 by Farsight Security, Inc.
+#!/usr/bin/env python
+
+# Copyright (c) 2018-2019, 2021 by Farsight Security, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- nmsg_input.pyx
+++ nmsg_input.pyx
@@ -1,6 +1,6 @@
 #cython: embedsignature=True
 
-# Copyright (c) 2009-2015, 2018-2019 by Farsight Security, Inc.
+# Copyright (c) 2009-2015, 2018-2021 by Farsight Security, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
